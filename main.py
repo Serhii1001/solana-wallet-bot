c39694cdcb86de264feedd6a1d8e4cb3dc0c9048
Serhii1001 <morrisdavid01993@gmail.com>
2025-05-13 10:52:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Отправь мне адрес Solana-кошелька.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Вы отправили: {message.text}")
+
+bot.polling()
